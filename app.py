cf3ba806acf6336519d8d26114859676953aa04b
ASTITVA BHARDWAJ <bastitva0@gmail.com>
2026-04-28 15:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 
 app = Flask(__name__, template_folder='.')
 
-DEFAULT_GEMINI_API_KEY = "AIzaSyDE2u4Wm3bR8NtzQQFR6ZgFERu_8JwXrCQ"
+DEFAULT_GEMINI_API_KEY = "AIzaSyDvdEPpPWh6gRJUG5dpCcn-dobE3-U3Vhc"
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', DEFAULT_GEMINI_API_KEY)
 
 genai.configure(api_key=GEMINI_API_KEY)
